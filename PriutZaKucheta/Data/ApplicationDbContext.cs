039bb56ba74fff7340aeeaf6ebe2cfb55c4d311a
Deo212 <d.stoyanov2017@pmg-pernik.com>
2022-03-23 09:35:47 +0200

    proekt 1

---
+++ PriutZaKucheta/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DogShelter.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriutZaKucheta.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
---
+++ PriutZaKucheta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PriutZaKucheta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
